ab02550eca8df4738135813ce09aa39350185bdc
Kota Yamada <k00ta.ymd@gmail.com>
2020-06-16 22:51:03 +0900

    how to pass data to/from parent/child

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,13 @@
   <div>
     <LikeHeader></LikeHeader>
     <h2>{{ number }}</h2>
-    <!-- 親コンポーネントから子にデータを渡すときは親のコンポーネントに子に渡す属性名を書く -->
-    <!-- DOMテンプレートではケバブケースでかく ここでもケバブが好ましい-->
-    <LikeNumberTag v-bind:totalNumber="number"></LikeNumberTag> 
-    <LikeNumberTag :total-number="number"></LikeNumberTag>
+    <!-- 親コンポーネントから子にデータを渡すときは子でpropsで定義したプロパティを属性名として親に書く -->
+    <!-- DOMテンプレートではケバブケースでかく ここでもtotal-numberなどケバブが好ましい-->
+    <!-- 子から親コンポーネントにデータを渡す時は受け取り役のv-onディレクティブと受け取りメソッドを親に書く。-->
+    <LikeNumberTag :total-number="number" @any-name-you-want="addChildData"></LikeNumberTag> 
+    <!-- またはでもOK。$eventは受け取ったデータ（子コンポーネント内の$emitの第2引数）を表す -->
+    <LikeNumberTag v-bind:totalNumber="number" @any-name-you-want="number = $event"></LikeNumberTag>
+    <LikeNumberTag :test-props="testText"></LikeNumberTag>
     <div id="app">
       <img alt="Vue logo" src="./assets/logo.png">
       <HelloWorld msg="Welcome to Your Vue.js App"/>
@@ -22,12 +25,20 @@ export default {
   name: 'App',
   data(){
     return {
-      number: 10
+      number: 10,
+      testText: 'this is props text defined in Parent by object'
     }
   },
   components: {
     HelloWorld,
     LikeHeader // 本来はLikeHeader: LikeHeaderの書き方だが、keyとvalueが同じなら省略して書ける
+  },
+  methods: {
+    // 子から親コンポーネントにデータを渡す時は受け取り役のv-onディレクティブと受け取りメソッドを親に書く
+    addChildData(value) {
+    // 子コンポーネント内の$emitの第2引数を"value"で受け取れる
+      this.number = value
+    }
   }
 }
 </script>
--- src/components/LikeNumber.vue
+++ src/components/LikeNumber.vue
@@ -1,18 +1,35 @@
 <template>
     <!-- template内には1つの要素でくくる必要がある -->
     <div>
-        <p>like! {{likedNumber/2}}</p>
+        <p>like! {{likedNumber}}</p>
+        <!-- 親コンポーネントから子にデータを渡すときは子にpropsとその中で書いたプロパティを書く -->
         <p>parent total number like! {{totalNumber}}</p>
         <p>half total number {{halfTotalNumber}}</p>
         <button @click="increment">+1</button>
+        <!-- 親コンポーネントから子にデータを渡すときは子にpropsとその中で書いたプロパティを書く -->
+        <p>{{testProps}}</p>
+        <p>Pass data to Parent component from child (= execute actinos)</p>
+        <!-- 子から親コンポーネントにデータを渡す時は渡し役のv-onとメソッドを子に書く -->
+        <button @click="passDataToParentFromChild">pass data to parent</button>
     </div>
 </template>
 
 <script>
 export default {
-    // 親コンポーネントから子にデータを渡すときは子にpropsを書く
-    // ここではキャメルケースが望ましい
-    props: ["totalNumber"],
+    // 親コンポーネントから子にデータを渡すときは子にpropsとその中で書いたプロパティを書く
+    // ここではキャメルケースが望ましい。が、親コンポーネントでのディレクティブではケバブが望ましい
+    // propsの書き方は配列とオブジェクトの2通り
+    // props: ["totalNumber", "testProps"],
+    props: {
+        totalNumber:{
+            type: Number,
+            default: 100
+        },
+        testProps: {
+            type: String,
+            default: 'this is default text for props by object'
+        }
+    },
     data: function(){
         return {
             likedNumber: 5
@@ -20,7 +37,14 @@ export default {
     },
     methods: {
         increment() {
-            this.likedNumber += 1
+            this.likedNumber += 1;
+            this.totalNumber += 1;
+        },
+        // 子から親コンポーネントにデータを渡す時は渡し役のv-onとメソッドを子に書く$emitを使う
+        passDataToParentFromChild(){
+            // $emitは本来はカスタムイベントを作成し子に反応して親でイベントを起こすということであり、データの受け渡しという意味での方法ではない
+            // Javascript（キャメルケースが基本）で使うタイミングがないからカスタムイベント名はケバブケース。
+            this.$emit('any-name-you-want', this.totalNumber + 1)
         }
     },
     computed:{
